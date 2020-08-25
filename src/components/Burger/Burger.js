1d969cc96cbd2beac1ba7a925f97e35c07667621
Noby707 <Noby707@users.noreply.github.com>
2020-08-25 13:46:33 -0400

    129. Outputting Burger Ingredient Dynamically && 130. Calculating the Ingredient Sum Dynamicall

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,36 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const burger = (props) => {
+  //   Object.keys(props.ingredients) will give us an array of the keys of props. ingredients, ie: [salad, veganbacon, cheese, meat]
+  //   Then Array(props.ingredients) gives us arrays containing empty/undefined elements. [ [], [], [,], [,], 1 1 2 2 elements.
+  //   The last part is just returning a component with
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  // Let us split the above thing for detail.
+  //   let IngredientsKeyArr = Object.keys(props.ingredients);
+  //   const transformedIngredients = IngredientsKeyArr.map((igKey) => {
+  //     let [...Array(props.ingredients[igKey])].map((_, i) => {
+  //       return <BurgerIngredient key={igKey + i} type={igKey} />;
+  //     });
+  //   });
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
